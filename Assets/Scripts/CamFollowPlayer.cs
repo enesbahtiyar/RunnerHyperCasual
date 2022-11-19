5c0f75d8ba6c79f5f89b899cb4610b03b5f05826
enesbahtiyar <enesbahtiyarserce@gmail.com>
2022-11-19 15:07:46 +0300

    Second Obstacle

--- Assets/Scripts/CamFollowPlayer.cs
+++ Assets/Scripts/CamFollowPlayer.cs
@@ -10,8 +10,7 @@ public class CamFollowPlayer : MonoBehaviour
     [SerializeField] Transform lookTarget;
     private void LateUpdate()
     {
-        Vector3 dPos = cameraTarget.position + dist;
-        Vector3 sPos = Vector3.Lerp(transform.position, dPos, speed * Time.deltaTime);
+        Vector3 dPos = cameraTarget.position + dist;        Vector3 sPos = Vector3.Lerp(transform.position, dPos, speed * Time.deltaTime);
         transform.position = sPos;
         transform.LookAt(lookTarget.position);
     }
